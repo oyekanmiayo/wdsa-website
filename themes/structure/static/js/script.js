ce780177b691fe1de6530c66f6f49f6a210f57e2
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2024-03-25 15:09:26 +0100

    fixed weird js bug

--- themes/structure/static/js/script.js
+++ themes/structure/static/js/script.js
@@ -1,67 +1,28 @@
+  const body = document.querySelector("body"),
+    nav = document.querySelector("nav"),
+    sidebarOpen = document.querySelector(".sidebarOpen"),
+    sidebarClose = document.querySelector(".sidebarClose");
 
-  if (window.location.pathname === "/cohorts") {
-    console.log("cohorts page");
-    const accordion = document.getElementsByClassName("accordion");
-    for (let i = 0; i < accordion.length; i++) {
-      accordion[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-    
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-          panel.style.maxHeight = null;
-        } else {
-          panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-      });
+  // script to toggle sidebar
+  sidebarOpen.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+
+  body.addEventListener("click", (e) => {
+    let clickedElement = e.target;
+    if (
+      !clickedElement.classList.contains("sidebarOpen") &&
+      !clickedElement.classList.contains("menu")
+    ) {
+      nav.classList.remove("active");
     }
-  }
+  });
 
-const body = document.querySelector("body"),
-  nav = document.querySelector("nav"),
-  sidebarOpen = document.querySelector(".sidebarOpen"),
-  sidebarClose = document.querySelector(".sidebarClose");
+  const navigation = document.querySelector(".menu");
+  const navigationHeight = navigation.offsetHeight;
 
-// script to toggle sidebar
-sidebarOpen.addEventListener("click", () => {
-  nav.classList.add("active");
-});
-
-body.addEventListener("click", (e) => {
-  let clickedElement = e.target;
-  if (
-    !clickedElement.classList.contains("sidebarOpen") &&
-    !clickedElement.classList.contains("menu")
-  ) {
-    nav.classList.remove("active");
-  }
-});
-
-const navigation = document.querySelector(".menu");
-const navigationHeight = navigation.offsetHeight;
-
-document.documentElement.style.setProperty(
-  "--scroll-padding",
-  navigationHeight + "px"
-);
-
-var join = document.getElementById("join-cohort");
-join.addEventListener("click", function () {
-  window.open("https://forms.gle/mXLRLAVggfByJWR17", "_blank");
-});
-
-var volunteer = document.getElementById("volunteer");
-volunteer.addEventListener("click", function () {
-  window.open("https://forms.gle/mXLRLAVggfByJWR17", "_blank");
-});
-
-var learn = document.getElementById("learn-more");
-learn.addEventListener("click", function () {
-  window.open("/schedule");
-});
-
-var read = document.getElementById("read-all");
-console.log(read);
-read.addEventListener("click", function () {
-  window.open("/testimonials");
-});
-
+  document.documentElement.style.setProperty(
+    "--scroll-padding",
+    navigationHeight + "px"
+  );
+  
